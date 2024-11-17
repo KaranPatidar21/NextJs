f7c66239547074b836f407bbed17fb7b3af0a08f
karan <patidarkaran33@gmail.com>
2024-11-17 23:43:20 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-"use client"
-import Link from 'next/link'
-import React, { useState } from 'react'
-import style from "../style/navbar.module.css"
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import style from "../style/navbar.module.css";
 function Navbar() {
   const [activeLink, setActiveLink] = useState(""); // Track active link
 
@@ -24,40 +24,40 @@ function Navbar() {
     //   </ul>
     // </nav>
     <nav className={`w-2/12 ${style.sideBar}`}>
-    <ul className={`${style.navItems}`}>
-      <li
-        className={`${style.navLists} ${
-          activeLink === "/dashboard" ? style.active : ""
-        }`}
-        onClick={() => handleLinkClick("/dashboard")}
-      >
+      <ul className={`${style.navItems}`}>
         <Link href="/dashboard" className={style.navLinks}>
-          Dashboard
+          <li
+            className={`${style.navLists} ${
+              activeLink === "/dashboard" ? style.active : ""
+            }`}
+            onClick={() => handleLinkClick("/dashboard")}
+          >
+            Dashboard
+          </li>
         </Link>
-      </li>
-      <li
-        className={`${style.navLists} ${
-          activeLink === "/skillstab" ? style.active : ""
-        }`}
-        onClick={() => handleLinkClick("/skillstab")}
-      >
         <Link href="/skillstab" className={style.navLinks}>
-          Skill Tab
-        </Link>
-      </li>
-      <li
-        className={`${style.navLists} ${
-          activeLink === "/internship" ? style.active : ""
-        }`}
-        onClick={() => handleLinkClick("/internship")}
-      >
-        <Link href="/internship" className={style.navLinks}>
-          Internship
-        </Link>
-      </li>
-    </ul>
-  </nav>
-  )
+        <li
+          className={`${style.navLists} ${
+            activeLink === "/skillstab" ? style.active : ""
+          }`}
+          onClick={() => handleLinkClick("/skillstab")}
+        >
+            Skill Tab
+        </li>
+          </Link>
+          <Link href="/internship" className={style.navLinks}>
+        <li
+          className={`${style.navLists} ${
+            activeLink === "/internship" ? style.active : ""
+          }`}
+          onClick={() => handleLinkClick("/internship")}
+        >
+            Internship
+        </li>
+          </Link>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
